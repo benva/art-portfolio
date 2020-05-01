9c23ab8d903d354e9296d6b70a8cb1e5812a5d93
Benjamin Verna-Asals <benjamin.asals@gmail.com>
2020-04-30 22:35:16 -0400

    Start of mobile responsiveness

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,10 @@
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Img from 'gatsby-image';
+
+import { phone } from './global-style';
 
 const Container = styled.header`
   text-transform: uppercase;
@@ -13,27 +16,88 @@ const Container = styled.header`
   }
 `;
 
-const LinkContainer = styled.div`
+const Links = styled.div`
+  @media (max-width: ${phone}) {
+    display: none;
+  }
+`;
+
+const MobileLinks = styled.div`
+  background-color: red;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const LinksContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
   width: 300px;
+
+  @media (max-width: ${phone}) {
+    flex-direction: column;
+    width: inherit;
+    text-align: center;
+    margin-top: 100px;
+  }
 `;
 
-const Header = ({ siteTitle }) => (
-  <Container>
-    <h1>
-      <Link to="/">{siteTitle}</Link>
-    </h1>
-    <LinkContainer>
+const Hamburger = styled.div`
+  cursor: pointer;
+  display: none;
+  position: fixed;
+  top: 28px;
+  right: 20px;
+  z-index: 1;
+
+  @media (max-width: ${phone}) {
+    display: block;
+  }
+`;
+
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      hamburger: file(relativePath: { eq: "header/hamburger.png" }) {
+        childImageSharp {
+          fixed(width: 32) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+
+  const renderLinks = () => (
+    <LinksContainer>
       <h3>
         <Link to="/about">About</Link>
       </h3>
       <h3>
         <Link to="/contact">Contact</Link>
       </h3>
-    </LinkContainer>
-  </Container>
-);
+    </LinksContainer>
+  );
+
+  return (
+    <Container>
+      <h1>
+        <Link to="/">{siteTitle}</Link>
+      </h1>
+      <Links>{renderLinks()}</Links>
+      <Hamburger onClick={toggleMenu}>
+        <Img alt="menu" fixed={data.hamburger.childImageSharp.fixed} />
+      </Hamburger>
+      {menuOpen && <MobileLinks open={menuOpen}>{renderLinks()}</MobileLinks>}
+    </Container>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
