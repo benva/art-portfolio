78e5915d17d2f9e605f2cc6d3bd8b97f098202ba
Benjamin Verna-Asals <benjamin.asals@gmail.com>
2020-05-02 15:22:12 -0400

    Other work pages

---
+++ src/pages/2015.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { twentyEighteen } from '../components/images';
+import ImageGallery from '../components/image-gallery';
+
+const TwentyFifteen = () => <ImageGallery images={twentyEighteen} year="2015" />;
+
+export default TwentyFifteen;
