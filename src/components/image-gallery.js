e2b704b15b204b57d6f5ca896ef12ec84199add5
Benjamin Verna-Asals <benjamin.asals@gmail.com>
2020-05-02 15:17:45 -0400

    Image gallery component

---
+++ src/components/image-gallery.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactImageGallery from 'react-image-gallery';
+import PropTypes from 'prop-types';
+
+import Layout from './layout';
+import SEO from './seo';
+
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const ImageGallery = ({ images, year }) => (
+  <Layout>
+    <SEO title={year} />
+    <Container>
+      <p>{year}</p>
+      <ReactImageGallery
+        autoPlay={false}
+        lazyLoad
+        items={images}
+        showFullscreenButton={false}
+        showPlayButton={false}
+      />
+    </Container>
+  </Layout>
+);
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(Object).isRequired,
+  year: PropTypes.string.isRequired,
+};
+
+export default ImageGallery;
