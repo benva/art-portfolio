c2f2ce6d9f778929082d2e678fe172c99c7ec23c
Benjamin Verna-Asals <benjamin.asals@gmail.com>
2020-05-12 20:45:37 -0400

    Add hover overlay on home page card

--- src/components/card.js
+++ src/components/card.js
@@ -9,6 +9,22 @@ const Container = styled.div`
   position: relative;
   margin-bottom: 50px;
 
+  &:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.2);
+    opacity: 0;
+    transition: opacity 0.5s ease;
+  }
+
+  &:hover:after {
+    opacity: 1;
+  }
+
   .year {
     position: absolute;
     top: 50%;
